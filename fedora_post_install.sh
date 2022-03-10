a4c1fa7933e15b394d67e5836287616984a4c250
Denis Maltsev <denmaltsev@gmail.com>
2022-03-10 21:44:24 +1000

    Добавлена установка колеков

--- fedora_post_install.sh
+++ fedora_post_install.sh
@@ -8,6 +8,10 @@ git config --global user.name "Denis Maltsev"
 # Установка диагностических утилит
 sudo dnf -y install neofetch inxi neofetch htop
 
+# Double commander
+sudo dnf copr enable vondruch/doublecmd
+sudo dnf install doublecmd-gtk
+
 # Проверка драйверов видеокарты
 neofetch; inxi -G; inxi -A; stat /
 
@@ -24,3 +28,12 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # установить утилиту контроля видеокарты от nwidia
 sudo dnf install gwe
+
+# установка видео кодеков @see: https://docs.fedoraproject.org/en-US/quick-docs/assembly_installing-plugins-for-playing-movies-and-music/
+# 1. добавление репозиториев
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# 2. устанавливаем мультимедиа кодеки
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+sudo dnf install lame\* --exclude=lame-devel
+sudo dnf group upgrade --with-optional Multimedia
